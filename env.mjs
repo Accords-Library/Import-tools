659be3eecb06f42ddabbfc195f9013e8c14a1fab
Nemesis <contact@keziahmoselle.fr>
2023-02-02 00:27:38 +0100

    fix: trailing slash in env definition

--- env.mjs
+++ env.mjs
@@ -2,7 +2,7 @@ import { str, envsafe, url } from 'envsafe';
 
 export const env = envsafe({
   STRAPI_BASE_API_URL: url({
-    devDefault: 'http://127.0.01:1337/api/',
+    devDefault: 'http://127.0.01:1337/api',
   }),
   STRAPI_API_TOKEN: str(),
 });
